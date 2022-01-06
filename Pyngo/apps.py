beeccbc50315f51ea68ee589b66fbc16c36c1ef4
joeliberal <joe.libral2693@gmail.com>
2022-01-06 17:31:00 +0330

    account translate base

--- Pyngo/apps.py
+++ Pyngo/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class PyngoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Pyngo'
+    verbose_name = _('pyngo')
+    verbose_name_plural = _('pyngo')
